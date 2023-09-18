c5350749a494eaa4e9359b84c34ad74ab42599e2
baglai <14839645+baglai@users.noreply.github.com>
2023-09-18 20:04:04 +0300

    test sb3

--- src/main/java/com/example/feigntracing/FeignTracingApplication.java
+++ src/main/java/com/example/feigntracing/FeignTracingApplication.java
@@ -2,11 +2,14 @@ package com.example.feigntracing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.core.publisher.Hooks;
 
 @SpringBootApplication
 public class FeignTracingApplication {
 
 	public static void main(String[] args) {
+
+		Hooks.enableAutomaticContextPropagation();
 		SpringApplication.run(FeignTracingApplication.class, args);
 	}
 
